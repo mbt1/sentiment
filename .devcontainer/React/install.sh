469b0d57c7d790910b59aa4bd50e59f2df146964
“mbt1” <mbt1@users.noreply.github.com>
2023-10-12 22:25:04 +0000

    starting to create react app

--- .devcontainer/React/install.sh
+++ .devcontainer/React/install.sh
@@ -3,3 +3,4 @@ npm install
 npm install -g react react-dom 
 npm install -g webpack webpack-dev-server webpack-cli
 npm install -g babel-core babel-loader babel-preset-env babel-preset-react babel-webpack-plugin
+npm install -g @babel/core @babel/preset-env @babel/preset-react babel-webpack-plugin babel-loader
