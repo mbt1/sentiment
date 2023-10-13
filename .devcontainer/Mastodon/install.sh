c775a2bd93171e094e24e00f83fa321092735c34
“mbt1” <mbt1@users.noreply.github.com>
2023-10-13 01:52:02 +0000

    started py API

---
+++ .devcontainer/Mastodon/install.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:$(dirname $(which pip))
+
+pip install Mastodon.py==1.8.1
--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from mastodon import Mastodon
+import os
+
+mastodon = Mastodon(
+    access_token=os.environ.get('MASTODON_SENTIMENT_MASTODON_API_KEY'),
+    api_base_url='https://mas.to/'  # Replace with the Mastodon instance URL
+)
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/api/public_toots', methods=['GET'])
+def get_public_toots():
+    public_toots = mastodon.timeline_public()
+    return jsonify(public_toots)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
