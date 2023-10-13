0a036d6de0c0cfc97da848401a9a4ec7b29b4f63
“mbt1” <mbt1@users.noreply.github.com>
2023-10-13 19:30:09 +0000

    separated installReactDependencies.sh for reusability on GitHub Actions.

--- .devcontainer/React/install.sh
+++ .devcontainer/React/install.sh
@@ -1,6 +1,2 @@
-export PATH=$PATH:$(dirname $(which npm))
-npm install
-npm install -g react react-dom 
-npm install -g webpack webpack-dev-server webpack-cli
-npm install -g @babel/core @babel/preset-env @babel/preset-react babel-webpack-plugin babel-loader
-npm install -g express
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+sh $SCRIPT_DIR/installReactDependencies.sh
