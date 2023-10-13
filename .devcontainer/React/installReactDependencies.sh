d55fcb147987c66aadbf3315348dea8738f5b3fe
“mbt1” <mbt1@users.noreply.github.com>
2023-10-13 20:46:09 +0000

    change the update of webpack to be global

--- .devcontainer/React/installReactDependencies.sh
+++ .devcontainer/React/installReactDependencies.sh
@@ -4,5 +4,5 @@ npm install -g react react-dom
 npm install -g webpack webpack-dev-server webpack-cli
 npm install -g @babel/core @babel/preset-env @babel/preset-react babel-webpack-plugin babel-loader
 npm install -g express
-npm update webpack
+npm update -g webpack 
 npm install -g copy-webpack-plugin
