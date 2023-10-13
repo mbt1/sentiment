c0adfa19b7c445b4aa6e268f798484e48caa4f68
“mbt1” <mbt1@users.noreply.github.com>
2023-10-13 20:59:07 +0000

    removing dependencies

--- .devcontainer/React/installReactDependencies.sh
+++ .devcontainer/React/installReactDependencies.sh
@@ -2,7 +2,6 @@ export PATH=$PATH:$(dirname $(which npm))
 npm install
 npm install -g react react-dom 
 npm install -g webpack webpack-dev-server webpack-cli
-npm install -g @babel/core @babel/preset-env @babel/preset-react babel-webpack-plugin babel-loader
+npm install -g @babel/core @babel/preset-env @babel/preset-react babel-loader
 npm install -g express
-npm update -g webpack 
 npm install -g copy-webpack-plugin
