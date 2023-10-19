0203a67b4d6519b59d5041b2359800c2037720ff
“mbt1” <mbt1@users.noreply.github.com>
2023-10-19 21:28:22 +0000

    attempt at cleaning code

--- backend/API-GetToots/GetToots.py
+++ backend/API-GetToots/GetToots.py
@@ -1,43 +1,10 @@
 import azure.functions as func
 from mastodon import Mastodon
-import os
 import json
 from datetime import datetime
-from azure.keyvault.secrets import SecretClient
-from azure.identity import DefaultAzureCredential
+import ReadEnvironment
 
-def get_mastodon_base_url():
-    # For local development on mac use these to set the variables
-    # launchctl setenv MASTODON_BASE_URL https://mastodon.world/
-    # for Azure use this in an Azure Cloud Shell
-    # az functionapp config appsettings set --name sentimentabot --resource-group Sentimentabot --settings "MASTODON_BASE_URL=https://mastodon.world/"
-    return os.environ.get("MASTODON_BASE_URL")
-
-def get_mastodon_api_key_in_azure():
-    # For local development on mac use these to set the variables
-    # launchctl setenv MASTODON_SENTIMENT_MASTODON_API_KEY [...]
-    # for Azure use this in an Azure Cloud Shell
-    # az functionapp config appsettings set --name sentimentabot --resource-group Sentimentabot --settings "KEY_VAULT_NAME=Sentimentabot-KV"
-
-    keyVaultSecretNameForAPIKey = "mastodon-api-key"
-    keyVaultName = os.environ.get("KEY_VAULT_NAME")
-    keyVaultUri = f"https://{keyVaultName}.vault.azure.net"
-
-    keyVaultCredential = DefaultAzureCredential()
-    keyVaultClient = SecretClient(vault_url=keyVaultUri, credential=keyVaultCredential)
-
-    print(f"Retrieving your {keyVaultSecretNameForAPIKey} from {keyVaultUri}.")
-    mastodonAPIKey = keyVaultClient.get_secret(keyVaultSecretNameForAPIKey)
-    return mastodonAPIKey.value
-
-def get_mastodon_api_key():
-    def use_environment_for_key():
-        return 'MASTODON_SENTIMENT_MASTODON_API_KEY' in os.environ
-
-    if use_environment_for_key():
-        return os.environ.get('MASTODON_SENTIMENT_MASTODON_API_KEY')
-    else:
-        return get_mastodon_api_key_in_azure()
+env_reader = ReadEnvironment()
 
 def my_serializer(o):
     if isinstance(o, datetime):
@@ -45,8 +12,8 @@ def my_serializer(o):
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     mastodon = Mastodon(
-        access_token= get_mastodon_api_key(),
-        api_base_url= get_mastodon_base_url()  
+        access_token= env_reader.mastodonAPIKey(),
+        api_base_url= env_reader.mastodonBaseURL()  
     )
     
     search_term = req.params.get('search_term')
---
+++ backend/API-GetToots/ReadEnvironment.py
@@ -0,0 +1,67 @@
+import os
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
+# For local development on mac use these to set the variables
+# launchctl setenv MASTODON_BASE_URL https://mastodon.world/
+# launchctl setenv MASTODON_SENTIMENT_MASTODON_API_KEY [...]
+
+# for Azure use this in an Azure Cloud Shell
+# az functionapp config appsettings set --name sentimentabot --resource-group Sentimentabot --settings "MASTODON_BASE_URL=https://mastodon.world/"
+# az functionapp config appsettings set --name sentimentabot --resource-group Sentimentabot --settings "KEY_VAULT_NAME=Sentimentabot-KV"
+
+class ReadEnvironment:
+    _MASTODON_API_KEY = None
+    _MASTODON_BASE_URL = None
+    _LANGUAGE_MODEL_ENDPOINT = None
+    _LANGUAGE_MODEL_KEY = None
+    _KEY_VAULT_NAME = None
+
+    _KEY_VAULT_SECRET_NAME_FOR_LANGUAGE_MODEL_KEY = "language-model-api-key"
+    _ENV_VARIABLE_NAME_FOR_LANGUAGE_MODEL_KEY = 'MASTODON_SENTIMENT_LANGUAGE_MODEL_KEY'
+    _ENV_VARIABLE_NAME_FOR_LANGUAGE_MODEL_ENDPOINT = 'MASTODON_SENTIMENT_LANGUAGE_MODEL_ENDPOINT'
+
+    _ENV_VARIABLE_NAME_FOR_MASTODON_API_KEY = 'MASTODON_SENTIMENT_MASTODON_API_KEY'
+    _ENV_VARIABLE_NAME_FOR_MASTODON_BASE_URL = 'MASTODON_BASE_URL'
+    _KEY_VAULT_SECRET_NAME_FOR_MASTODON_API_KEY = "mastodon-api-key"
+
+    _ENV_VARIABLE_NAME_FOR_KEY_VAULT_NAME = 'MASTODON_SENTIMENT_KEY_VAULT_NAME'
+
+    def _get_environment_variable(self, key):
+        return os.environ.get(key)
+
+    def _get_secret(self, secret_name):
+        keyVaultUri = f"https://{self._KEY_VAULT_NAME}.vault.azure.net"
+        keyVaultCredential = DefaultAzureCredential()
+        keyVaultClient = SecretClient(vault_url=keyVaultUri, credential=keyVaultCredential)
+
+        mastodonAPIKey = keyVaultClient.get_secret(secret_name)
+        return mastodonAPIKey.value
+
+    def _use_keyvault(self):
+        return self._ENV_VARIABLE_NAME_FOR_KEY_VAULT_NAME in os.environ
+
+    def _get_secret_locally_or_vault(self, env_variable_name, secret_name):
+        if self._use_keyvault():
+            return self._get_secret(secret_name)
+        else:
+            return self._get_environment_variable(env_variable_name)
+
+    def __init__(self):
+        self._KEY_VAULT_NAME = self._get_environment_variable(self._ENV_VARIABLE_NAME_FOR_KEY_VAULT_NAME)
+        self._MASTODON_BASE_URL = self._get_environment_variable(self._ENV_VARIABLE_NAME_FOR_MASTODON_BASE_URL)
+        self._MASTODON_API_KEY = self._get_secret_locally_or_vault(self._ENV_VARIABLE_NAME_FOR_MASTODON_API_KEY, self._ENV_VARIABLE_NAME_FOR_MASTODON_API_KEY)
+        # self._LANGUAGE_MODEL_ENDPOINT = self._get_environment_variable(self._ENV_VARIABLE_NAME_FOR_LANGUAGE_MODEL_ENDPOINT)
+        # self._LANGUAGE_MODEL_KEY = self._get_secret_locally_or_vault(self._ENV_VARIABLE_NAME_FOR_LANGUAGE_MODEL_KEY, self._KEY_VAULT_SECRET_NAME_FOR_LANGUAGE_MODEL_KEY)
+
+    def mastodonBaseURL(self):
+        return self._MASTODON_BASE_URL
+    
+    def mastodonAPIKey(self):
+        return self._MASTODON_API_KEY
+    
+    def languageModelKey(self):
+        return self._LANGUAGE_MODEL_KEY
+    
+    def languageModelEndpoint(self):
+        return self._LANGUAGE_MODEL_ENDPOINT
