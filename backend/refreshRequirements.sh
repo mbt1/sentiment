d66807f2f6df979fa51c6bbb78d702464a5b1984
“mbt1” <mbt1@users.noreply.github.com>
2023-11-01 02:06:44 +0000

    added startDevServer code, added SVG background

---
+++ backend/refreshRequirements.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the script's directory
+cd "$(dirname "$0")"
+
+# Install packages from requirements.txt
+pip install -r requirements.txt
---
+++ frontend/startDevServer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the script's directory
+cd "$(dirname "$0")"
+
+# Start Dev Server
+npm run buildserve
